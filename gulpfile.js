e064e31d2061f64bf7a4326a323f758395d4d8e6
Roman <romanchebotarenko@gmail.com>
2022-06-06 00:52:55 +0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+//Основной модуль
+import gulp from "gulp";
+//Импорт путей
+import { path } from "./gulp/config/path.js";
+
+//Передаем значения в глобальную переменную
+global.app = {
+  path: path,
+  gulp: gulp
+};
+
+//Импорт задач
+import { copy } from "./gulp/tasks/copy.js";
+
+//Выполнение сценария по умолчанию
+gulp.task("default", copy);
