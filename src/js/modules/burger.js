d785ca69a46d3e20fbf0e85a0cb20425ce6df927
Roman <romanchebotarenko@gmail.com>
2022-07-06 19:18:12 +0300

    111

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -1,24 +1,47 @@
 const burgerTogglerOpen = document.querySelector('.burger-toggler--open');
 const burgerTogglerClose = document.querySelector('.burger-toggler--close');
 const burgerMenu = document.querySelector('.nav-main');
-//const burgerMenuOverlay = document.querySelector('.burger-menu-overlay');
-//const burgerTogglerIcon = document.querySelector('.burger-toggler-icon');
-/* const burgerTogglerIconClosed = document.querySelector(
-  '.burger-toggler-icon-closed'
-); */
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, { once: true });
+  });
+
+function addBackdrop() {
+  const addBackdrop = document.createElement('div');
+  addBackdrop.id = 'backdrop';
+  document.body.appendChild(addBackdrop);
+}
+
+function removeBackdrop() {
+  const removeBackdropEl = document.getElementById('backdrop');
+  removeBackdropEl.remove();
+}
 
 function openBurgerMenu() {
   document.body.style.overflow = 'hidden';
   burgerMenu.classList.add('_active');
-  burgerMenu.classList.add('animate__animated');
-  burgerMenu.classList.add('animate__slideInDown');
+  addBackdrop();
 }
+// TODO REMOVE BACKDROP ON CLICK
 
 function closeBurgerMenu() {
   document.body.style.removeProperty('overflow');
-   burgerMenu.classList.remove('animate__slideInDown');
-  burgerMenu.classList.add('animate__slideInUp');
   burgerMenu.classList.remove('_active');
+  removeBackdrop();
 }
 
 window.addEventListener('resize', resizeListener);
@@ -32,17 +55,15 @@ function resizeListener() {
 if (burgerTogglerOpen) {
   burgerTogglerOpen.addEventListener('click', function () {
     openBurgerMenu();
+    animateCSS('.nav-main', 'slideInDown');
+    animateCSS('.nav-main', 'faster');
   });
 }
 
 if (burgerTogglerClose) {
   burgerTogglerClose.addEventListener('click', function () {
     closeBurgerMenu();
+    animateCSS('.nav-main', 'slideOutUp');
+    animateCSS('.nav-main', 'faster');
   });
 }
-
-/* if (burgerMenuOverlay) {
-  burgerMenuOverlay.addEventListener('click', function () {
-    closeBurgerMenu();
-  });
-} */
