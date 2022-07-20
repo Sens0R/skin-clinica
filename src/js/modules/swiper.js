db8b4839dbb1f9d59979f84d4dff5653d4b11a3f
Roman <romanchebotarenko@gmail.com>
2022-07-20 15:09:54 +0300

    2131

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,27 +1,38 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, {
+  Navigation,
+  Pagination,
+  Keyboard,
+  Autoplay,
+ 
+} from 'swiper';
 
-
- const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper', {
   // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-   // Optional parameters
-   direction: 'horizontal',
-   loop: true,
+  modules: [Navigation, Pagination, Keyboard, Autoplay],
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  speed: 500,
+  autoplay: {
+    delay: 4000,
+  },
  
-   // If we need pagination
-   pagination: {
-     el: '.swiper-pagination',
-   },
- 
-   // Navigation arrows
-   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-   },
- 
-   // And if we need scrollbar
-   scrollbar: {
-     el: '.swiper-scrollbar',
-    
-   },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-btn--left',
+    prevEl: '.swiper-btn--right',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyVisible: true,
+    pageUpDown: true,
+  },
 });
