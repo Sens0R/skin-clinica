594624bb40a34586e33bb5f1500162f1b7ed6173
Roman <romanchebotarenko@gmail.com>
2022-08-26 15:09:39 +0300

    321

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,56 +1,67 @@
-export function accordion(accordionBtn, name = 'accordion') {
-  const accordionButtons = document.querySelectorAll(accordionBtn);
+export function accordion() {
+  let accordionInstances = document.querySelectorAll('.accordion');
 
-  accordionButtons.forEach((accordionButton, i) => {
-    const content = accordionButton.nextElementSibling;
-    let contentHeight;
+  accordionInstances.forEach((accordionInstance, i) => {
+    let instanceNumber;
+    let accordionInstanceName = '';
 
-    accordionButton.ariaExpanded = 'false';
-    content.ariaHidden = true;
-    const contentId = `${name}-collapse-${i + 1}`;
-    content.id = contentId;
-    accordionButton.setAttribute('aria-controls', contentId);
-    console.log(content);
-    console.log(accordionButton);
+    if (i > 0) {
+      instanceNumber = '-' + (i + 1) + '-';
+      accordionInstanceName = 'accordion';
+    } else instanceNumber = '';
 
-    const mql = window.matchMedia('(orientation: landscape)');
-    mql.addEventListener('change', changeHandler);
+    const contentIdPrefix = accordionInstanceName + instanceNumber;
+    const accordionButtons =
+      accordionInstance.querySelectorAll('.accordion__button');
 
-    function changeHandler(e) {
-      if (accordionButton.classList.contains('_active'))
-        if (e.matches) {
-          contentHeight = content.scrollHeight;
-          content.style.maxHeight = `${contentHeight}px`;
-        } else {
-          contentHeight = content.scrollHeight;
-          content.style.maxHeight = `${contentHeight}px`;
+    accordionButtons.forEach((accordionButton, i) => {
+      const content = accordionButton.nextElementSibling;
+      let contentHeight;
+      accordionButton.ariaExpanded = 'false';
+      content.ariaHidden = true;
+      const contentId = `${contentIdPrefix}collapse-${i + 1}`;
+      content.id = contentId;
+      accordionButton.setAttribute('aria-controls', contentId);
+
+      const mql = window.matchMedia('(orientation: landscape)');
+      mql.addEventListener('change', changeHandler);
+
+      function changeHandler(e) {
+        if (accordionButton.classList.contains('_active'))
+          if (e.matches) {
+            contentHeight = content.scrollHeight;
+            content.style.maxHeight = `${contentHeight}px`;
+          } else {
+            contentHeight = content.scrollHeight;
+            content.style.maxHeight = `${contentHeight}px`;
+          }
+      }
+
+      accordionButton.addEventListener('click', accordionBtnToggler);
+      accordionButton.addEventListener('keyup', () => {
+        if (key === 'Enter' || key === 'Space') {
+          accordionBtnToggler();
         }
-    }
+      });
 
-    accordionButton.addEventListener('click', accordionBtnToggler);
-    accordionButton.addEventListener('keyup', () => {
-      if (key === 'Enter' || key === 'Space') {
-        accordionBtnToggler();
+      function accordionBtnToggler() {
+        if (accordionButton.classList.contains('_active')) {
+          accordionButton.classList.remove('_active');
+
+          accordionButton.ariaExpanded = 'false';
+          content.ariaHidden = true;
+
+          content.style.maxHeight = null;
+          return;
+        }
+
+        contentHeight = content.scrollHeight;
+        accordionButton.classList.add('_active');
+
+        accordionButton.ariaExpanded = 'true';
+        content.ariaHidden = false;
+        content.style.maxHeight = `${contentHeight}px`;
       }
     });
-
-    function accordionBtnToggler() {
-      if (accordionButton.classList.contains('_active')) {
-        accordionButton.classList.remove('_active');
-
-        accordionButton.ariaExpanded = 'false';
-        content.ariaHidden = true;
-
-        content.style.maxHeight = null;
-        return;
-      }
-
-      contentHeight = content.scrollHeight;
-      accordionButton.classList.add('_active');
-
-      accordionButton.ariaExpanded = 'true';
-      content.ariaHidden = false;
-      content.style.maxHeight = `${contentHeight}px`;
-    }
   });
 }
